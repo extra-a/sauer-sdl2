a99417a68c65107a7f303d0109ab5808ef2b81ae
aaa <aaa@aaa.local>
2014-10-22 21:04:27 +0400

    New version.

--- engine/main.cpp
+++ engine/main.cpp
@@ -1355,7 +1355,7 @@ int main(int argc, char **argv)
     inputgrab(grabinput = true);
     ignoremousemotion();
 
-    conoutf(stringify_macro(\f0Sauerbraten SDL2 Client\f1 Version 1.3.1));
+    conoutf(stringify_macro(\f0Sauerbraten SDL2 Client\f1 Version 1.3.2));
 
     for(;;)
     {
