9435a0829a5527f630e1317f7df6c64389ec3596
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2015-02-06 15:57:07 +0000

    init message tweak
    git-svn-id: svn://svn.code.sf.net/p/sauerbraten/code/src@5078 07af1644-1164-4067-b897-63bf4dc9b748

--- engine/main.cpp
+++ engine/main.cpp
@@ -1292,7 +1292,7 @@ int main(int argc, char **argv)
     inbetweenframes = true;
     renderbackground("initializing...");
 
-    logoutf("init: gl: effects");
+    logoutf("init: effects");
     loadshaders();
     particleinit();
     initdecals();
