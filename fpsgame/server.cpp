23cdf9b3bf721d958a6613c061d18f5a76db8b00
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2018-01-14 20:35:06 +0000

    vote fix
    git-svn-id: https://svn.code.sf.net/p/sauerbraten/code/src@5380 07af1644-1164-4067-b897-63bf4dc9b748

--- fpsgame/server.cpp
+++ fpsgame/server.cpp
@@ -3196,6 +3196,7 @@ namespace server
             {
                 getstring(text, p);
                 filtertext(text, text, false);
+                fixmapname(text);
                 int reqmode = getint(p);
                 vote(text, reqmode, sender);
                 break;
